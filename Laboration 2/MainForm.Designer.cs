f44e8dec9ae122c7d9c172ecffab4b11f8ef40b8
Deltaximity <45205838+Deltaximity@users.noreply.github.com>
2023-02-17 23:11:21 +0100

    Initial commit

---
+++ Laboration 2/MainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Laboration_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AntalDragningar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lotto1 = new System.Windows.Forms.TextBox();
+            this.lotto2 = new System.Windows.Forms.TextBox();
+            this.lotto3 = new System.Windows.Forms.TextBox();
+            this.lotto4 = new System.Windows.Forms.TextBox();
+            this.lotto5 = new System.Windows.Forms.TextBox();
+            this.lotto6 = new System.Windows.Forms.TextBox();
+            this.lotto7 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.matchBox5 = new System.Windows.Forms.TextBox();
+            this.matchBox6 = new System.Windows.Forms.TextBox();
+            this.matchBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Din lottorad:";
+            // 
+            // AntalDragningar
+            // 
+            this.AntalDragningar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.AntalDragningar.Location = new System.Drawing.Point(228, 52);
+            this.AntalDragningar.Name = "AntalDragningar";
+            this.AntalDragningar.Size = new System.Drawing.Size(137, 20);
+            this.AntalDragningar.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Antal dragningar:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(371, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Starta Lotto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartaLotto);
+            // 
+            // lotto1
+            // 
+            this.lotto1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto1.Location = new System.Drawing.Point(87, 12);
+            this.lotto1.Name = "lotto1";
+            this.lotto1.Size = new System.Drawing.Size(65, 20);
+            this.lotto1.TabIndex = 9;
+            // 
+            // lotto2
+            // 
+            this.lotto2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto2.Location = new System.Drawing.Point(158, 12);
+            this.lotto2.Name = "lotto2";
+            this.lotto2.Size = new System.Drawing.Size(65, 20);
+            this.lotto2.TabIndex = 10;
+            // 
+            // lotto3
+            // 
+            this.lotto3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto3.Location = new System.Drawing.Point(229, 12);
+            this.lotto3.Name = "lotto3";
+            this.lotto3.Size = new System.Drawing.Size(65, 20);
+            this.lotto3.TabIndex = 11;
+            // 
+            // lotto4
+            // 
+            this.lotto4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto4.Location = new System.Drawing.Point(300, 12);
+            this.lotto4.Name = "lotto4";
+            this.lotto4.Size = new System.Drawing.Size(65, 20);
+            this.lotto4.TabIndex = 12;
+            // 
+            // lotto5
+            // 
+            this.lotto5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto5.Location = new System.Drawing.Point(372, 12);
+            this.lotto5.Name = "lotto5";
+            this.lotto5.Size = new System.Drawing.Size(65, 20);
+            this.lotto5.TabIndex = 13;
+            // 
+            // lotto6
+            // 
+            this.lotto6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto6.Location = new System.Drawing.Point(444, 12);
+            this.lotto6.Name = "lotto6";
+            this.lotto6.Size = new System.Drawing.Size(65, 20);
+            this.lotto6.TabIndex = 14;
+            // 
+            // lotto7
+            // 
+            this.lotto7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lotto7.Location = new System.Drawing.Point(515, 12);
+            this.lotto7.Name = "lotto7";
+            this.lotto7.Size = new System.Drawing.Size(65, 20);
+            this.lotto7.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "5 rätt:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "6 rätt:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(369, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "7 rätt:";
+            // 
+            // matchBox5
+            // 
+            this.matchBox5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.matchBox5.Location = new System.Drawing.Point(155, 93);
+            this.matchBox5.Name = "matchBox5";
+            this.matchBox5.ReadOnly = true;
+            this.matchBox5.Size = new System.Drawing.Size(65, 20);
+            this.matchBox5.TabIndex = 19;
+            // 
+            // matchBox6
+            // 
+            this.matchBox6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.matchBox6.Location = new System.Drawing.Point(283, 93);
+            this.matchBox6.Name = "matchBox6";
+            this.matchBox6.ReadOnly = true;
+            this.matchBox6.Size = new System.Drawing.Size(65, 20);
+            this.matchBox6.TabIndex = 20;
+            // 
+            // matchBox7
+            // 
+            this.matchBox7.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.matchBox7.Location = new System.Drawing.Point(406, 93);
+            this.matchBox7.Name = "matchBox7";
+            this.matchBox7.ReadOnly = true;
+            this.matchBox7.Size = new System.Drawing.Size(65, 20);
+            this.matchBox7.TabIndex = 21;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 126);
+            this.Controls.Add(this.matchBox7);
+            this.Controls.Add(this.matchBox6);
+            this.Controls.Add(this.matchBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lotto7);
+            this.Controls.Add(this.lotto6);
+            this.Controls.Add(this.lotto5);
+            this.Controls.Add(this.lotto4);
+            this.Controls.Add(this.lotto3);
+            this.Controls.Add(this.lotto2);
+            this.Controls.Add(this.lotto1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AntalDragningar);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(611, 165);
+            this.Name = "MainForm";
+            this.Text = "Lottorad";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AntalDragningar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lotto1;
+        private System.Windows.Forms.TextBox lotto2;
+        private System.Windows.Forms.TextBox lotto3;
+        private System.Windows.Forms.TextBox lotto4;
+        private System.Windows.Forms.TextBox lotto5;
+        private System.Windows.Forms.TextBox lotto6;
+        private System.Windows.Forms.TextBox lotto7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox matchBox5;
+        private System.Windows.Forms.TextBox matchBox6;
+        private System.Windows.Forms.TextBox matchBox7;
+    }
+}
+
---
+++ Laboration 2/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboration_2
+{
+    public partial class MainForm : Form
+    {
+        private int[] row = new int[7];
+        private int[] userRow = new int[7];
+        private int fiveScore = 0;
+        private int sixScore = 0;
+        private int sevenScore = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Metod som körs när man trycker på "Starta Lotto"
+        private void StartaLotto(object sender, EventArgs e)
+        {
+            int antal = 0;
+
+            try {
+                antal = int.Parse(AntalDragningar.Text);
+                userRow[0] = Convert.ToInt32(lotto1.Text);
+                userRow[1] = Convert.ToInt32(lotto2.Text);
+                userRow[2] = Convert.ToInt32(lotto3.Text);
+                userRow[3] = Convert.ToInt32(lotto4.Text);
+                userRow[4] = Convert.ToInt32(lotto5.Text);
+                userRow[5] = Convert.ToInt32(lotto6.Text);
+                userRow[6] = Convert.ToInt32(lotto7.Text);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (antal <= 0)
+            {
+                MessageBox.Show("Antal dragningar måste vara minst 1");
+                return;
+            }
+
+            if (userRow[0] < 1 || userRow[0] > 35 || userRow[1] < 1 || userRow[1] > 35 || userRow[2] < 1 || userRow[2] > 35 ||
+                userRow[3] < 1 || userRow[3] > 35 || userRow[4] < 1 || userRow[4] > 35 || userRow[5] < 1 || userRow[5] > 35 ||
+                userRow[6] < 1 || userRow[6] > 35)
+            {
+                MessageBox.Show("Lottorad talen måste vara mellan (inklusiv) 1-35");
+                return;
+            } else if (userRow.Length != userRow.Distinct().Count()) {
+                MessageBox.Show("Talen måste vara unika! (inga dubletter)");
+                return;
+            }
+
+            for (int i = 0; i < antal; i++)
+            {
+                GenerateNewRow();
+                CheckForMatches();
+            }
+        }
+
+        // Genererar en lottorad (7 unika tal mellan (inklusiv) 1-35)
+        Random r = new Random();
+        private void GenerateNewRow() 
+        {
+            // Genererar och lagrar 7 slumpmässiga tal mellan 1-35 i en array
+            for (int i = 0; i < row.Length; i++)
+            {
+                int slump = r.Next(1, 36);
+                row[i] = slump;
+            }
+
+            // Kontrollerar om det finns dubletter
+            for (int i = row.Length - 1; i > 0; i--) 
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    if (row[i] == row[j])
+                    {
+                        GenerateNewRow();
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Jämför arrayen vi har genererat med talen användaren har angivit
+        private void CheckForMatches()
+        {
+            int matches = 0;
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                for (int j = 0; j < userRow.Length; j++)
+                {
+                    if (row[i] == userRow[j])
+                    {
+                        matches++;
+                    }
+                }
+            }
+
+            if (matches == 5) fiveScore++;
+            if (matches == 6) sixScore++;
+            if (matches == 7) sevenScore++;
+
+            matchBox5.Text = fiveScore.ToString();
+            matchBox6.Text = sixScore.ToString();
+            matchBox7.Text = sevenScore.ToString();
+        }
+    }
+}
